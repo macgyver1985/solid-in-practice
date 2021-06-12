80386c15a52744e47d02696d4a7fbc3588a26adf
macgyver1985 <felipe.augusto.franca@gmail.com>
2021-06-11 23:49:22 -0300

    Inclusão do código incorreto na execução da main.

--- csharp/Main/Program.cs
+++ csharp/Main/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Solid.SingleResponsibilityPrinciple.Incorrect;
+using Incorrect = Solid.SingleResponsibilityPrinciple.Incorrect;
 
 namespace main
 {
@@ -7,9 +7,28 @@ namespace main
     {
         static void Main(string[] args)
         {
-            NewsletterService service = new NewsletterService();
+            Console.WriteLine("##################################################################");
+            Console.WriteLine("# Execução do código que viola o Single Responsibility Principle #");
+            Console.WriteLine("##################################################################");
+            Console.WriteLine(".");
+            Console.WriteLine("Informe o e-mail para se registrar na newsletter.");
 
-            service.Register("teste@teste.com.br");
+            string email = Console.ReadLine();
+
+            Console.WriteLine(".");
+
+            try
+            {
+                Incorrect.NewsletterService service = new Incorrect.NewsletterService();
+
+                service.Register(email);
+
+                Console.WriteLine("Registro realizado com sucesso.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ocorreu um erro inesperado.");
+            }
         }
     }
 }
